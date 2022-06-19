98e8d4338052f49aaaebe97d4f90447d3bfe2507
anarodrigues1996 <anadira.dira@gmail.com>
2022-06-19 22:58:13 -0100

    image in header

--- pages/index.js
+++ pages/index.js
@@ -9,30 +9,32 @@ export default function Home() {
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" />
       </Head>
       <header className='vh-100'>
-        <nav className="navbar navbar-expand-sm">
+        <nav className="navbar navbar-expand-sm position-absolute w-100">
 
           <div className="container">
 
             <a href="#">
-              <img src="/img/logo-in8-dev.svg" height="35"/>
+              <img src="/img/logo-in8-dev.svg" height="35" />
             </a>
             <ul className="navbar-nav ml-auto">
               <li className="nav-item">
-                <a className="nav-link" href="#">cadastro</a>
+                <a className="nav-link text-white" href="#">cadastro</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">lista</a>
+                <a className="nav-link text-white" href="#">lista</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">sobre mim</a>
+                <a className="nav-link text-white" href="#">sobre mim</a>
               </li>
             </ul>
           </div>
 
         </nav>
-        <div className='container'>
-          <h1 className={styles.fontHu}>ESTAGIO</h1>
-          <h2>PROVA DE SELECAO</h2>
+        <div className='container d-flex flex-wrap align-content-center h-100'>
+          <div>
+            <h1 className={styles.fontHu}>ESTÁGIO</h1>
+            <h2>PROVA DE SELEÇÃO</h2>
+          </div>
         </div>
       </header>
       <section className='bg-info'>
