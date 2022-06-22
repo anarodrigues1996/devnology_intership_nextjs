f20a376e30de02d7fc4d457bce843f988f96967a
anarodrigues1996 <anadira.dira@gmail.com>
2022-06-22 22:55:51 -0100

    responsive menu

--- pages/NavBar.js
+++ pages/NavBar.js
@@ -1,23 +1,50 @@
+import { useState } from 'react'
 
 export default function NavBar() {
+
+    const [menu, setMenu] = useState(true);
+
+    function toogleMenu()
+    {
+      setMenu(!menu)
+    }
+
     return (
         <nav className="navbar navbar-expand-sm position-absolute w-100">
           <div className="container">
 
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar" onClick={toogleMenu}>
+            {
+              !menu &&
+              <img src="/img/hamburguer.svg" height="24" />
+            }
+            {
+              menu &&
+              <img src="/img/hamburguer-aberto0.svg" height="24" />
+            }
+          </button>
+
             <a href="#">
               <img src="/img/logo-in8-dev.svg" height="35" />
             </a>
-            <ul className="navbar-nav ml-auto">
-              <li className="nav-item">
-                <a className="nav-link text-white" href="#signUp">cadastro</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link text-white" href="#list">lista</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link text-white" href="#aboutme">sobre mim</a>
-              </li>
-            </ul>
+
+            {
+              menu &&
+              <div className="ml-auto myMenu" id="collapsibleNavbar">
+              <ul className="navbar-nav">
+                <li className="nav-item">
+                  <a className="nav-link text-white" href="#signUp">cadastro</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link text-white" href="#list">lista</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link text-white" href="#aboutme">sobre mim</a>
+                </li>
+              </ul>
+              </div>
+            }
+
           </div>
 
         </nav>
